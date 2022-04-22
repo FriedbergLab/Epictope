8037ba7fcc5bfa0b30a69f94de8992ff6b25bb72
hchung@iastate.edu <0280733+henrichung@users.noreply.github.com>
2022-04-22 13:42:47 -0500

    download pdb files from alphafold

--- code/epitope.py
+++ code/epitope.py
@@ -1,202 +1,107 @@
+#!/usr/bin/python3
+from epitope_helper import *
 import pandas
-import re
-import os
-import selenium
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 import requests
-from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from Bio.Blast import NCBIXML
-from Bio import AlignIO
-from Bio.Align.Applications import MuscleCommandline
-from Bio.Blast.Applications import NcbiblastpCommandline
+import re
+import argparse
+import sys
+import csv
 
-# read in transcription factors from file
-filename = "../data/tfs.txt"
-tf = pandas.read_csv(filename)
+def query_to_alphafold(filename, species):
 
+    #Check if output folder exists.
+    does_folder_exist("outputs")
 
-# pull uniprot ID from alphafold search
-
-def render(url, driver_loc = '/usr/bin/chromedriver'):
-	s = Service(driver_loc)
-	opts = Options()
-	opts.add_argument("--disable-extensions")
-	opts.add_argument("--disable-gpu")
-	opts.add_argument("--no-sandbox") # linux only
-	opts.add_argument("--headless")
-	browser = webdriver.Chrome(service=s, options = opts)
-	browser.get(url)
-	soup = BeautifulSoup(browser.page_source, features = "html.parser")
-	browser.quit()
-	return(soup)
-
-def fetch_uniprot(query, organism, server = "https://alphafold.ebi.ac.uk/search/text/"):
-	if organism != "" :
-		organism_ext = "?organismScientificName=" + re.sub(" ", "%20", organism)
-	else:
-		organism_ext = ""
-	alphafold_url = server + query + organism_ext
-	res = render(alphafold_url)
-	return(res)
-
-uniprot_pages = [fetch_uniprot(p, "Danio rerio") for p in tf['geneName']]
-
-def search_link(soup, link = "http://www.uniprot.org/uniprot/.*"):
-	text = re.search(link, soup.prettify()).group()
-	url = re.sub(" target=_blank>", "", text.replace("\"", "")) + ".fasta"
-	return(url)
-
-def fetch_text(url):
-	res = requests.get(url).text
-	return(res)
-
-uniprot_links = list(map(search_link, uniprot_pages))
-
-fastas = list(map(fetch_text, uniprot_links))
-
-
-named_fastas = dict(zip(tf['geneName'], fastas))
-
-seq_list = []
-for key, value in named_fastas.items():
-	seq = re.search("[A-Z]{10,}", value.replace("\n", "")).group()
-	rec = SeqRecord(Seq(seq), 
-                id = key, 
-                name = key, 
-                description = "")
-	seq_list.append(rec)
-
-SeqIO.write(seq_list, "blast_queries.fasta", "fasta")
-
-def list_files(folder, filetype):
-    folder_list = []
-    for dirpath, dirnames, filenames in os.walk(folder):
-        for filename in [f for f in filenames if f.endswith(filetype)]:
-            folder_list.append(os.path.join(dirpath, filename))
-    return folder_list
-
-def multi_blast(db_path, fasta_file, evalue = 1e-3, outfmt = 5):
-    db_list = list_files(db_path, ".fa")
-    # Loop to blast query fasta against any databbases in db folder.
-    for db in db_list:
-        blastp_cline = NcbiblastpCommandline(
-            query = fasta_file, 
-            db = db, 
-            evalue = evalue, 
-            outfmt = outfmt, 
-            out = "../outputs/" + db.split("/")[3] + "_blast.xml",)
-        stdout, stderr  = blastp_cline()
-
-
-multi_blast("../data/blastdb", "blast_queries.fasta", evalue = 1e-3, outfmt = 5)
-
-
-def xml_parse(xmlpath):
-    # Initate ensembleRest object and lists
-    id_list = []
-    # Parse records from xml output.
-    for record in NCBIXML.parse(open(xmlpath)): 
-        try:
-            id_temp = record.alignments[0].title.split(" ")[0].split(".")[0]
-        # If not hit from blast query, will return IndexError.  
-        # Append "" if no hit to maintain index order of queries.
-        except IndexError:
-            id_temp = ""
-        id_list.append(id_temp)
-    return(id_list)
-
-#Outputs: list object of fetched sequences as string.
-def fetch_transcripts(query, 
-    server = "https://rest.ensembl.org", 
-    ext = "/sequence/id/", 
-    seq_type = "cds",
-    content_type = "fasta",
-    multi_seq = "1"
-):
-    url = server + ext + query + \
-        "?multiple_sequences=" + multi_seq + \
-        ";type=" + seq_type + \
-        ";content_type=" + content_type
-    r = requests.get(url, allow_redirects = True, headers = {'Content-type': 'text/x-fasta'})
+    #read in input file.
+    tf = pandas.read_csv(filename)
     
-    if re.search("error", r.text):
-        print("No sequence found for " + query)
-        transcripts = ""
-    else:
-        transcripts = str(r.text).split(">")[1:]
-    return(transcripts)
+    # fetch uniprot sequence from AlphaFold website
+    uniprot_pages = [fetch_uniprot(p, species) for p in tf
 
-def get_max_str(lst):
-    return max(lst, key=len) if lst else ""
+    #fetch transcripts from ensembl API.
+    ts = list(map(fetch_transcripts, tf['ensemblID']))
+    
+    #select longest sequence.
+    max_ts = list(map(get_max_str, ts)) 
 
-def as_list(arg):
-    return arg if isinstance(arg, list) else [arg]
+    #convert string to SeqIO fasta object.
+    fastas = list(map(string_to_fasta, max_ts))
 
-def string_to_fasta(string, desc = "", name = ""):
-    if string == "":
-        rec = SeqRecord(Seq(""), id = "", name = name, description = desc)
-    else:  
-        temp_string = string.replace("\n", "")
-        seq_id = re.search("ENS[A-Z]+[0-9]+", temp_string).group()
-        seq = re.search("[A-Z]{10,}", temp_string).group()
-        rec = SeqRecord(Seq(seq), 
-                id = seq_id, 
-                name = name, 
-                description = desc)
-    return(rec)
+    #add gene names to fasta object.
+    blast_queries0 = name_fastas(fastas, tf["geneName"])
 
-#parse blast xml output and fetch full transcripts for hits.
-output_list = []
-for file in list_files("../outputs", ".xml"):
-    print("parsing " + file)
-    seqs = list(map(fetch_transcripts, xml_parse(file)))
-    max_seqs = list(map(get_max_str, as_list(seqs)))
-    fastas = list(map(string_to_fasta, max_seqs))
-    output_list.append(fastas)
+    #filter out queries where no transcript was returned.
+    blast_queries = [ele for ele in blast_queries0 if str(ele.seq)!=""]
 
-#combine matching transcripts.
-matches = tuple(zip(seq_list, *output_list))
+    #Write blast queries to file.
+    SeqIO.write(blast_queries, "blastqueries.fasta", "fasta")
+
+    #blast against folder of databases.
+    multi_blast("../data/blastdbs", "blastqueries.fasta", evalue = 1e-3, outfmt = 5)
 
 
-def string_strip(string):
-	res = string.split("/")[2].split("_blast")[0]
-	return(res)
+    #parse blast xml output and fetch full transcripts for hits.
+    output_list = []
+    for file in list_files("outputs", ".xml"):
+        print("parsing " + file)
+        seqs = list(map(fetch_transcripts, xml_parse(file)))
+        max_seqs = list(map(get_max_str, as_list(seqs)))
+        fastas = list(map(string_to_fasta, max_seqs))
+        output_list.append(fastas)
 
-descriptions = list(map(string_strip, list_files("../outputs", ".xml")))
-descriptions.insert(0, "Danio_rerio")
+    #combine matching transcripts.
+    matches = tuple(zip(blast_queries, *output_list))
 
-def desc_fastas(fastas, desc):
-    desc_fastas = []
-    for fasta in fastas:
-        i = 0
-        for entry in fasta:
-                entry.description = desc[i]
-                i += 1
-        desc_fastas.append(fasta)
-    return(desc_fastas)   
+    descriptions = "Danio_renio", "Xenopus_tropicalis", "Mus_musculus", "Gallus_gallus", "Homo_sapiens", "Takifugu_rubripes"]
+    desc_matches = desc_fastas(matches, descriptions)
+    #write matching transcripts to individual fastas.
+    #using protein sequence
+    to_fasta(desc_matches, protein = True)
 
-desc_matches = desc_fastas(matches, descriptions)
-#write matching transcripts to individual fastas.
-#using protein sequence
-def to_fasta(seq_list, name_dict = ""):
-   # Write matching sequences to fastas for msa.
-    for match in seq_list:
-        filtered_match = list(filter(None, match))
-        if len(filtered_match) > 1:
-                SeqIO.write(filtered_match, "../outputs/" + match[0].name + ".fasta", "fasta")
-to_fasta(desc_matches)
+    #MSA with muscle.
+    for fasta in list_files("outputs", ".fasta"):
+        muscle_cline = MuscleCommandline(
+        input=fasta, 
+        clw = True, 
+        out=fasta.split(".fasta")[0] + ".clw")
+        stdout, stderr = muscle_cline()
 
-#MSA with muscle.
-for fasta in list_files("../outputs", ".fasta"):
-    muscle_cline = MuscleCommandline(
-    input=fasta, 
-    clw = True, 
-    out=fasta.split(".fasta")[0] + ".clw")
-    stdout, stderr = muscle_cline()
+   	#parse muscle txt output and calculate shannon entropy for each column.
+   	#write result to csv file.
+    with open('outputs/entropies.csv', 'w', encoding='UTF8', newline='') as f:
+    	writer = csv.writer(f)
+    	for file in list_files("outputs", ".clw"):
+        	#print("calculating entropy of" + file)
+        	res = shannon_entropy_list_msa(AlignIO.read(file, "clustal"))
+        	res.insert(0, file.split("/")[1].split(".")[0])
+        	writer.writerow(res)
+    f.close()
 
+    print("No results returned for:")
+    for i in neg_indices:
+        print(tf.geneName[i])
+        	
+
+
+###
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description= 
+    'Takes an input file of genes and associated Ensembl IDs, retrieves \
+    associated sequences from the Ensembl REST API, BLASTs the sequences against \
+    local blasteable databases, and then performs a MSA with the homologous sequences.')
+
+    parser.add_argument("-i", "--input", dest="filename", required=True,
+                    help="single column text file with gene name queries", metavar="txt file",
+                    type=lambda x: is_valid_file(parser, x))
+    parser.add_argument("-c", "--config", dest="config", required = True, 
+                    help = "path to config.cfg file with parameter values", metavar="cfg file",
+                    default = "code/config.yml",
+                    type=lambda x: is_valid_file(parser, x))
+
+    args = parser.parse_args() 
+
+    with open(args.config, 'r') as file:
+        params = yaml.safe_load(file)
+
+    query_to_msa(args.filename, species)
 
--- code/epitope_helper.py
+++ code/epitope_helper.py
@@ -4,26 +4,151 @@ import Bio
 from Bio import SeqIO
 from Bio import AlignIO
 from Bio.Align.Applications import MuscleCommandline
-from Bio.Blast.Applications import NcbiblastnCommandline
+from Bio.Blast.Applications import NcbiblastpCommandline
 from Bio.Blast import NCBIXML
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
-#from ensemblrest import EnsemblRest
+import configparser
 import re
 import requests
 import os
 import os.path
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import requests
+import time
+import yaml
+
+# Loads a dynamic webpage.
+def render(url, driver_loc = 'code/chromedriver_linux64/chromedriver'):
+    """Loads a dynamic webpage
+    using chromedriver
+    """
+    s = Service(driver_loc)
+    opts = Options()
+    opts.add_argument("--disable-extensions")
+    opts.add_argument("--disable-gpu")
+    opts.add_argument("--no-sandbox") # linux only
+    opts.add_argument("--headless")
+    browser = webdriver.Chrome(service=s, options = opts)
+    browser.get(url)
+    time.sleep(10)
+    soup = BeautifulSoup(browser.page_source, features = "lxml")
+    browser.quit()
+    return(soup)
+
+def search_alphafold(query, organism, server = "https://alphafold.ebi.ac.uk/search/text/"):
+    """Searches Alphafold database
+    for protein query, optional argument for 
+    species.
+    """
+    if organism != "" :
+        organism_ext = "?organismScientificName=" + re.sub(" ", "%20", organism)
+    else:
+        organism_ext = ""
+    alphafold_url = server + query + organism_ext
+    res = render(alphafold_url)
+    return(res)
 
 
-#Fetch sequences using ensembl API.  
-#Inputs:
-#server - server url.
-#ex - resource extension.
-#seq_type - type of sequence.  
-#content_type - return data format.  
-#multi_seq - boolean for return multiple sequences.
+def fetch_content(url):
+    """retrives content
+    from URL
+    """
+    res = requests.get(url, allow_redirects = True)
+    return(res.content)
 
-#Outputs: list object of fetched sequences as string.
+def search_link(soup, link):
+    """Searches a beautifulsoup object
+    for a link and returns matches.
+    """
+    all_links = ""
+    for a in soup.find_all('a', href = True):
+        all_links += a['href']+" "
+    search = re.search(link, all_links)
+    if search is None: return("No result found")
+    return(search.group())
+
+def delete_multiple_element(list_object, indices):
+    """Deletes elements of a list given a list and
+    a list of elements to delete.
+    """
+    indices = sorted(indices, reverse=True)
+    for idx in indices:
+        if idx < len(list_object):
+            list_object.pop(idx)
+
+def does_folder_exist(folder, create = False):
+    """ Checks if folder exists,
+    optional argument to create file if 
+    desired.
+    """
+    if os.path.exists(folder) == False:
+        print(folder+" does not exist")
+        if create == True:
+            os.mkdir(folder)
+            print("Created " + folder + " folder.")
+    else:
+        print(folder+" exists.")
+
+
+def is_valid_file(parser, arg):
+    """checks if file path exists.
+    """
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return open(arg, 'r')  # return an open file handle
+
+def list_files(folder, filetype):
+    """lists all of the files
+    of a specific filetype in a 
+    given folder.
+    """
+    folder_list = []
+    for dirpath, dirnames, filenames in os.walk(folder):
+        for filename in [f for f in filenames if f.endswith(filetype)]:
+            folder_list.append(os.path.join(dirpath, filename))
+    return folder_list
+
+
+def multi_blast(db_path, fasta_file, evalue = 1e-3, outfmt = 5):
+    """performs a blast search for each
+    sequence listed in a fasta file against a
+    database of organisms.
+    """
+    db_list = list_files(db_path, ".fa")
+    # Loop to blast query fasta against any databases in db folder.
+    for db in db_list:
+        blastp_cline = NcbiblastpCommandline(
+            query = fasta_file, 
+            db = db, 
+            evalue = evalue, 
+            outfmt = outfmt, 
+            out = "outputs/" + db.split("/")[3] + "_blast.xml",)
+        stdout, stderr  = blastp_cline()
+
+def xml_parse(xmlpath):
+    """Parses the xml output from
+    NCBI blast output.
+    """
+    # Initate ensembleRest object and lists
+    id_list = []
+    # Parse records from xml output.
+    for record in NCBIXML.parse(open(xmlpath)): 
+        try:
+            id_temp = record.alignments[0].title.split(" ")[0].split(".")[0]
+        # If not hit from blast query, will return IndexError.  
+        # Append "" if no hit to maintain index order of queries.
+        except IndexError:
+            id_temp = ""
+        id_list.append(id_temp)
+    return(id_list)
+
+
 def fetch_transcripts(query, 
     server = "https://rest.ensembl.org", 
     ext = "/sequence/id/", 
@@ -31,6 +156,9 @@ def fetch_transcripts(query,
     content_type = "fasta",
     multi_seq = "1"
 ):
+    """fetch the coding sequence
+    for a protein from ensembl
+    """
     url = server + ext + query + \
         "?multiple_sequences=" + multi_seq + \
         ";type=" + seq_type + \
@@ -45,27 +173,44 @@ def fetch_transcripts(query,
     return(transcripts)
 
 
-#function to return max element length from a list.  
-#Input: list.
-#Output: longest element from input list.
 def get_max_str(lst):
+    """determine which string 
+    is longest from a list of strings
+    """
     return max(lst, key=len) if lst else ""
 
-#function to add name attribute to list of SeqIO objects.  
-#Input: 
-#fastas - list of SeqIO objects.  
-#names - list of names (string).  
-#Output: list of SeqIO objects with edited name attribute.
-def name_fastas(fastas, names):
-    i = 0
-    named_fastas = []
-    for fasta in fastas:
-        fasta.name = names[i]
-        i += 1
-        named_fastas.append(fasta)
-    return(named_fastas)    
+def as_list(arg):
+    """convert an object 
+    to a list.
+    """
+    return arg if isinstance(arg, list) else [arg]
+
+def string_to_fasta(string, desc = "", name = ""):
+    """converts a string object
+    to a Bio.SeqRecord object
+    """
+    if string == "":
+        rec = SeqRecord(Seq(""), id = "", name = name, description = desc)
+    else:  
+        temp_string = string.replace("\n", "")
+        seq_id = re.search("ENS[A-Z]+[0-9]+", temp_string).group()
+        seq = re.search("[A-Z]{10,}", temp_string).group()
+        rec = SeqRecord(Seq(seq), 
+                id = seq_id, 
+                name = name, 
+                description = desc)
+    return(rec)
+
+def string_strip(string):
+    """strip the folder argument from a string.
+    """
+    res = string.split("/")[2].split("_blast")[0]
+    return(res)
 
 def desc_fastas(fastas, desc):
+    """adds a description attribute to 
+    fasta SeqRecord objects
+    """
     desc_fastas = []
     for fasta in fastas:
         i = 0
@@ -73,128 +218,28 @@ def desc_fastas(fastas, desc):
                 entry.description = desc[i]
                 i += 1
         desc_fastas.append(fasta)
-    return(desc_fastas)    
+    return(desc_fastas)   
 
 
-
-#function that identifies all files of specific type in a folder
-#Inputs:
-#folder - folder to search through.  
-#filetype - type of file to search for.  
-#Outputs: list of filenames of specified type in folder.
-def list_files(folder, filetype):
-    folder_list = []
-    for dirpath, dirnames, filenames in os.walk(folder):
-        for filename in [f for f in filenames if f.endswith(filetype)]:
-            folder_list.append(os.path.join(dirpath, filename))
-    return folder_list
-
-
-#function to perform blast against local databases
-#Inputs:
-#db_path - path to local reference databases.
-#fasta_file - fasta file of blast queries.
-#evalue - tuning parameter for stringency of blast search.
-#outfmt - blast output file format (5 = xml)
-#Outputs - xml files with blast results
-def multi_blast(db_path, fasta_file, evalue = 1e-3, outfmt = 5):
-    db_list = list_files(db_path, ".fa")
-    # Loop to blast query fasta against any databbases in db folder.
-    for db in db_list:
-        blastn_cline = NcbiblastnCommandline(
-            query = fasta_file, 
-            db = db, 
-            evalue = evalue, 
-            outfmt = outfmt, 
-            word_size = 11,
-            gapopen = 2,
-            gapextend = 2,
-            reward = 1, 
-            penalty = -3, 
-            out = "outputs/" + db.split("/")[3] + "_blast.xml",)
-        stdout, stderr  = blastn_cline()
-
-#function to return inputs as objects as single element lists.
-#Input: object
-#Output: single element list
-def as_list(arg):
-    return arg if isinstance(arg, list) else [arg]
-
-#function to parse blast xml output files
-#Input: path to xml file.
-#Output: list of first hits from blast query.
-def xml_parse(xmlpath):
-    # Initate ensembleRest object and lists
-    id_list = []
-    # Parse records from xml output.
-    for record in NCBIXML.parse(open(xmlpath)): 
-        try:
-            id_temp = record.alignments[0].title.split(" ")[1].split(".")[0]
-        # If not hit from blast query, will return IndexError.  
-        # Append "" if no hit to maintain index order of queries.
-        except IndexError:
-            id_temp = ""
-        id_list.append(id_temp)
-    return(id_list)
-
-#function to write list of SeqIO objects to fasta file with
-#option to convert nucleotide sequence to protein sequence. 
-#Input
-#Output
 def to_fasta(seq_list, protein = False, name_dict = ""):
    # Write matching sequences to fastas for msa.
     for match in seq_list:
         filtered_match = list(filter(None, match))
         if len(filtered_match) > 1:
+            if protein == False:
                 SeqIO.write(filtered_match, 
-                    "outputs/" + match[0].name + ".fasta", "fasta")
+                    params["data_file"] + match[0].name + ".fasta", "fasta")
             else:
                 for newseq in filtered_match: 
                     temp = newseq.translate()
                     newseq.seq = temp.seq
                 SeqIO.write(filtered_match,
-                    "outputs/" + match[0].name + ".fasta", "fasta")
-
-#function to convert a character string consisting of fasta information into a 
-#Biopython SeqIO object.
-#Input: character string consisting of fasta ID and sequence.
-#Output Biopython SeqIO object.
-def string_to_fasta(string, desc = "", name = ""):
-    if string == "":
-        rec = SeqRecord(Seq(""), id = "", name = name, description = desc)
-    else:  
-        temp_string = string.replace("\n", "")
-        seq_id = re.search("ENS[A-Z]+[0-9]+", temp_string).group()
-        seq = re.search("[A,C,T,G]{3,}", temp_string).group()
-        rec = SeqRecord(Seq(seq), 
-                id = seq_id, 
-                name = name, 
-                description = desc)
-    return(rec)
-
-#function to determine if an input file exists.
-#Input:  a filename
-#Output: opens file if exists, reports if file does not exist. 
-def is_valid_file(parser, arg):
-    if not os.path.exists(arg):
-        parser.error("The file %s does not exist!" % arg)
-    else:
-        return open(arg, 'r')  # return an open file handle
-
-#function to determine if output folder exists, if it does not exist
-#create the output folder.
-#Input: folder name
-#Output: No output if folder exists, creates folder if does not exist.
-def does_folder_exist(folder):
-    if os.path.exists(folder) == False:
-        os.mkdir(folder)
-        print("Created " + folder + " folder.")
+                    params["data_file"] + match[0].name + ".fasta", "fasta")
 
 
-# Function to calcuate the Shannon's entropy per alignment column
-# H=-\sum_{i=1}^{M} P_i\,log_2\,P_i 
-# Gaps and N's are included in the calculation
 def shannon_entropy(list_input):
+    """Function to calcuate the Shannon's entropy per alignment column
+    """
     import math
     unique_base = set(list_input)                           # Get only the unique bases in a column
     #unique_base = unique_base.discard("-")
@@ -209,8 +254,10 @@ def shannon_entropy(list_input):
     sh_entropy = -(sum(entropy_list))
     return sh_entropy
 
-#function to calculate shannon entropy for all columns in a MSA.
+
 def shannon_entropy_list_msa(alignment_file):
+    """Function to calculate shannon entropy for all columns in a MSA.
+    """
     shannon_entropy_list = []
     for col_no in range(len(list(alignment_file[0]))):
         list_input = list(alignment_file[:, col_no])
