19dacf185f4fcfdb5f7f929a88e6c576f651b690
Henri Chung <30280733+henrichung@users.noreply.github.com>
2023-07-28 22:26:09 -0500

    update installation images

--- install/windows/environment_install.bat
+++ install/windows/environment_install.bat
@@ -1,10 +1,7 @@
 @echo off
 conda config --set always_yes yes
 conda activate epictope
-conda install -c bioconda blast
-conda install -c bioconda muscle
-conda install -c salilab dssp
-conda install -c conda-forge r-base
-conda install -c conda-forge r-stringi
-conda install -c conda-forge r-openssl
+conda install -c bioconda blast muscle
+conda install -c speleo3 dssp
+conda install -c conda-forge r-base r-stringi r-openssl
 conda config --set always_yes no
