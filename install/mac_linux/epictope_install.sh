95fafd35c8e125d565871dc33d02902df7fc9a4c
Henri Chung <30280733+henrichung@users.noreply.github.com>
2023-09-15 15:42:48 -0500

    Update epictope_install.sh

--- install/mac_linux/epictope_install.sh
+++ install/mac_linux/epictope_install.sh
@@ -28,21 +28,23 @@ if ! command -v conda &> /dev/null; then
     echo "Conda installed successfully. Please close and restart your terminal."
 else
     echo "Conda is already installed."
+
+    # Create a new conda environment for epictope
+    conda create -n epictope
+    conda activate epictope
+    conda install -c bioconda blast muscle
+    conda install -c salilab dssp
+    conda install -c anaconda openssl>=3.0.9
+    conda install -c conda-forge r-base r-stringi r-openssl r-remotes python>=3.11.4
+    
+    # Install R packages
+    R -e "remotes::install_github('henrichung/epitope_tag')"
+    # Install epitope_tag scripts
+    curl "https://raw.githubusercontent.com/henrichung/epitope_tag/main/scripts/single_score.R" 
+    curl "https://raw.githubusercontent.com/henrichung/epitope_tag/main/scripts/plot_scores.R" 
+    curl "https://raw.githubusercontent.com/henrichung/epitope_tag/main/scripts/install.R"
+    curl "https://raw.githubusercontent.com/henrichung/epitope_tag/main/scripts/config_defaults.R" 
 fi
 
 
-# Create a new conda environment for epictope
-conda create -n epictope
-conda activate epictope
-conda install -c bioconda blast muscle
-conda install -c salilab dssp
-conda install -c anaconda openssl>=3.0.9
-conda install -c conda-forge r-base r-stringi r-openssl r-remotes python>=3.11.4
 
-# Install R packages
-R -e "remotes::install_github('henrichung/epitope_tag')"
-# Install epitope_tag scripts
-wget "https://raw.githubusercontent.com/henrichung/epitope_tag/main/scripts/single_score.R" 
-wget "https://raw.githubusercontent.com/henrichung/epitope_tag/main/scripts/plot_scores.R" 
-wget "https://raw.githubusercontent.com/henrichung/epitope_tag/main/scripts/install.R"
-wget "https://raw.githubusercontent.com/henrichung/epitope_tag/main/scripts/config_defaults.R" 
