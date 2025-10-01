aa3bf9e00b973bf9b63c8075938552b1000b160e
Parnal Joshi <44910482+parnaljoshi@users.noreply.github.com>
2025-10-01 13:54:54 -0500

    Change dssp installation to use phenix-project channel

--- install/windows/epictope_install.bat
+++ install/windows/epictope_install.bat
@@ -14,7 +14,7 @@ if %errorlevel% neq 0 (
 )
 :: activate the 'Epictope' environment and install dependencies
 call conda activate epictope
-call conda install -c speleo3 dssp
+call conda install phenix-project::dssp
 call conda install -c conda-forge r-base r-stringi r-openssl r-remotes
 :: report success
 echo Epictope environment installation complete.
